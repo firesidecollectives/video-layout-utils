0630a3edc00018342f1133b618553c18bccf7b82
Igor Šćekić <iscekic@protonmail.com>
2021-12-01 11:58:17 +0100

    fix no-unused-vars rule

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,3 +1,24 @@
+export declare type DimensionKey = "p" | "l" | "pp" | "pl" | "lp" | "ll" | "ppp" | "ppl" | "plp" | "pll" | "lpp" | "lpl" | "llp" | "lll" | "pppp" | "pppl" | "pplp" | "ppll" | "plpp" | "plpl" | "pllp" | "plll" | "lppp" | "lppl" | "lplp" | "lpll" | "llpp" | "llpl" | "lllp" | "llll";
+declare type DimensionCalculatorInput = {
+    containerHeight: number;
+    containerWidth: number;
+    insets: {
+        top: number;
+        right: number;
+        bottom: number;
+        left: number;
+    };
+    isLandscapeRight: boolean;
+};
+declare type DimensionCalculatorOutput = {
+    height: number;
+    width: number;
+    top?: number;
+    left?: number;
+    nameTop?: number;
+    nameLeft?: number;
+};
+export declare type DimensionCalculator = (input: DimensionCalculatorInput) => DimensionCalculatorOutput[];
 export declare type Dimensions = {
     width: number;
     height: number;
@@ -5,3 +26,4 @@ export declare type Dimensions = {
 export declare const getCenterOffset: (containerSize: number, videoSize: number) => number;
 export declare const findLargestPortraitBox: ({ height: containerHeight, width: containerWidth, }: Dimensions) => Dimensions;
 export declare const findLargestLandscapeBox: ({ height: containerHeight, width: containerWidth, }: Dimensions) => Dimensions;
+export {};
